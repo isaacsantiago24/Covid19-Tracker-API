b7868e3a4007bd0826c8550520b05f1da7230b23
Isaac Santiago <62231878+isaacsantiago24@users.noreply.github.com>
2021-01-09 22:18:03 -0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import Columns from "react-columns";
+import Form from "react-bootstrap/form";
+
+// import './App.css';
 
 function App() {
+  const [latest, setLatest] = useState([]); //array format
+  const [results, setResults] = useState([]);
+  const [searchCountries, setSearchCountries] = useState("");
+
+  useEffect(() => {
+    axios
+      .all([
+        axios.get("https://corona.lmao.ninja/v3/covid-19/all"), //getting info from api
+        axios.get("https://corona.lmao.ninja/v3/covid-19/countries"), //getting country info
+      ])
+
+      .then((responseArr) => {
+        //if successful
+        setLatest(responseArr[0].data); //set to latest
+        setResults(responseArr[1].data); //using the country api
+      })
+
+      .catch((err) => {
+        console.log(err); //if not then display the error
+      });
+  }, []);
+
+  const date = new Date(parseInt(latest.updated));
+  const lastUpdated = date.toString(); //convert to a string
+
+  const filterCountries = results.filter((item) => {
+    return searchCountries !== ""
+      ? item.country.includes(searchCountries)
+      : item;
+  });
+
+  const countries = filterCountries.map((data, i) => {
+    //looping
+    return (
+      <Card
+        key={i}
+        bg="light"
+        text="dark"
+        className="text-center"
+        style={{ margin: "10px" }}
+      >
+        <Card.Img variant="top" src={data.countryInfo.flag} />
+        <Card.Body>
+          <Card.Title>{data.country}</Card.Title>
+          <Card.Text>Cases {data.cases}</Card.Text>
+          <Card.Text>Deaths {data.deaths}</Card.Text>
+          <Card.Text>Recovered {data.recovered}</Card.Text>
+          <Card.Text>Today's cases {data.todayCases}</Card.Text>
+          <Card.Text>Today's deaths {data.todayDeaths}</Card.Text>
+          <Card.Text>Active {data.active}</Card.Text>
+          <Card.Text>Critical {data.critical}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  });
+
+  var queries = [
+    {
+      columns: 3,
+      query: "min-width:500px",
+    },
+    {
+      columns: 3,
+      query: "min-width: 900px;",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <br />
+      <h2 style={{ textAlign: "center" }}> Covid-19 Live Stats</h2>
+      <br />
+      <CardDeck>
+        <Card
+          bg="secondary"
+          text="white"
+          className="text-center"
+          style={{ margin: "12px" }}
         >
-          Learn React
-        </a>
-      </header>
+          <Card.Body>
+            <Card.Title>Cases</Card.Title>
+            <Card.Text>{latest.cases}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated {lastUpdated}</small>
+          </Card.Footer>
+        </Card>
+
+        <Card
+          bg="danger"
+          text="white"
+          className="text-center"
+          style={{ margin: "12px" }}
+        >
+          <Card.Body>
+            <Card.Title>Death</Card.Title>
+            <Card.Text>{latest.deaths}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated {lastUpdated}</small>
+          </Card.Footer>
+        </Card>
+
+        <Card
+          bg="success"
+          text="white"
+          className="text-center"
+          style={{ margin: "12px" }}
+        >
+          <Card.Body>
+            <Card.Title>Recovered</Card.Title>
+            <Card.Text>{latest.recovered}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated {lastUpdated}</small>
+          </Card.Footer>
+        </Card>
+      </CardDeck>
+      <br></br>
+
+      <Form>
+        <Form.Group controlId="formGroupSearch">
+          <Form.Control
+            type="text"
+            placeholder="Search a country"
+            onChange={(e) => setSearchCountries(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+
+      <Columns queries={queries}>{countries}</Columns>
     </div>
   );
 }
